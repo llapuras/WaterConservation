98b266271e44f1973c285f4e371865397e79da62
LLapuras <chenwanwan13@gmail.com>
2020-08-16 03:40:39 +0800

    add

---
+++ Assets/Scripts/Task/HideGo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideGo : MonoBehaviour
+{
+    public GameObject[] gos;
+    // Start is called before the first frame update
+    public void TurnOffFountain()
+    {
+        foreach (GameObject x in gos)
+        {
+            x.SetActive(false);
+        }
+    }
+}

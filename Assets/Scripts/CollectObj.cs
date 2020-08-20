21208fd2309fc01aa994a138a3df1680d24dc6bd
LLapuras <chenwanwan13@gmail.com>
2020-08-20 18:15:41 +0800

    add

---
+++ Assets/Scripts/CollectObj.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CollectObj : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject goal;
+
+    public GameObject bubble;
+
+    public float detectDistance = 1;
+
+    public GameObject collectUI;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector3.Distance(player.transform.position, transform.position);
+
+        Debug.Log(dist);
+        if (dist < detectDistance)
+        {
+            bubble.SetActive(true);
+            GetCollected();
+        }
+        else
+        {
+            bubble.SetActive(false);
+        }
+    }
+
+    void GetCollected()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            collectUI.GetComponent<LaundryTaskManager>().addPiles();
+            goal.SetActive(false);
+            gameObject.SetActive(false);
+
+        }
+    }
+
+}
---
+++ Assets/Scripts/LaundryTaskManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LaundryTaskManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int collected = 0;
+    public int totalPiles = 5;
+    public Text display;
+
+    private bool isReady = false;
+
+    public GameObject dialogue01;
+    public GameObject dialogue02;
+    public GameObject collectingUI;
+
+    public GameObject collectTrigger;
+
+
+    public void CheckCollecting()
+    {
+        if(collected == totalPiles)
+        {
+            isReady = true;
+            dialogue01.SetActive(false);
+            dialogue02.SetActive(true);
+            dialogue02.GetComponent<DialogueTrigger>().TriggerDialogue();
+            collectingUI.SetActive(false);
+        }
+        else
+        {
+            isReady = false;
+            dialogue02.SetActive(false);
+            dialogue01.SetActive(true);
+            dialogue01.GetComponent<DialogueTrigger>().TriggerDialogue();
+            collectingUI.SetActive(true);
+
+            collectTrigger.SetActive(true);
+        }
+    }
+
+    public void addPiles()
+    {
+        collected += 1;
+        display.text = collected + " / 5";
+    }
+
+
+    void Start()
+    {
+        display.text = "0 / 5";
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
